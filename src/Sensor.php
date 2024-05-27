64cfb15f7285334f9c19206365f01a0f3ce428a2
Sebastian Utbult <sebastian.utbult@awardit.com>
2024-05-27 10:44:04 +0200

    Add Sensor objects (getters & constructor)

--- src/Sensor.php
+++ src/Sensor.php
@@ -6,19 +6,38 @@ use Acheron\DB;
 
 class Sensor
 {
-    public $id;
-    public $name;
-    public $lng;
-    public $lat;
-    public $status;
-    public $battery;
+    public function __construct(
+        public int $id,
+        public string $name,
+        public float $lng,
+        public float $lat,
+        public string $status,
+        public $battery = null
+    ) {}
 
-    public static function getById()
-    {
-    }
-
-    public static function getAll()
+    public static function getById(int $id): Sensor
     {
         $db = new DB();
+        $sql = "SELECT  id, 
+                        name, 
+                        lat, 
+                        lng, 
+                        battery_level as battery, 
+                        status
+                        FROM sensors
+                        WHERE id = " . (int)$id;
+        $result = $db->get($sql)[0];
+        return new Sensor(...$result);
+    }
+
+    public static function getAll(): array
+    {
+        $db = new DB();
+        $sql = "SELECT id, name, lat, lng, battery_level as battery, status FROM sensors ORDER BY name ASC";
+        $result = $db->get($sql);
+        foreach ($result as $sensor) {
+            $sensors[] = new Sensor(...$sensor);
+        }
+        return $sensors;
     }
 }
